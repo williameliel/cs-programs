139b4be0d56c16c4ad54225adc2f6201cbbb2672
Joel Fernandes <agnel.joel@gmail.com>
2016-05-29 00:09:35 -0700

    Add my_free and merging logic
    Signed-off-by: Joel Fernandes <agnel.joel@gmail.com>

--- misc/malloc.c
+++ misc/malloc.c
@@ -12,7 +12,6 @@ char mem[MAX_HEAP_SIZE];
 void *my_malloc(int size)
 {
 	struct hdr *cur, *frag;
-	int not_found = 0;
 	int alloc_size;
 
 	cur = (struct hdr *)mem;
@@ -41,14 +40,42 @@ void *my_malloc(int size)
 	return (void *)((char *)cur + sizeof(struct hdr));
 }
 
+void my_free(void *ptr)
+{
+	struct hdr *cur, *prev;
+	struct hdr *hdr = (struct hdr *)((char *)ptr - sizeof(struct hdr));
+	hdr->valid = 0;
+
+	cur = (struct hdr *)((char *)hdr + sizeof(struct hdr) + hdr->size);
+	prev = hdr;
+	while (1) {
+		if ((char *)cur - mem >= MAX_HEAP_SIZE)
+			break;
+
+		if (cur->valid) {
+			break;
+		} else {
+			/*
+			 * Merge consecutive blocks that are free
+			 */
+			prev->size = prev->size + sizeof(struct hdr) + cur->size;
+			cur = (struct hdr *)((char *)cur + sizeof(struct hdr) + cur->size);
+		}
+	}
+}
+
 int main(void)
 {
 	struct hdr *hdr = (struct hdr *)mem;
+	void *ptr;
+
 	hdr->size = MAX_HEAP_SIZE - sizeof(struct hdr);
-	printf("1: %p\n", my_malloc(30));
-	printf("2: %p\n", my_malloc(60));
-	printf("3: %p\n", my_malloc(10000));
+
+	printf("1: %p\n", my_malloc(50));
+	ptr = my_malloc(60);
+	printf("2: %p\n", ptr);
+	printf("3: %p\n", my_malloc(10));
+	my_free(ptr);
 	printf("3: %p\n", my_malloc(1));
+	printf("3: %p\n", my_malloc(50));
 }
-
-
